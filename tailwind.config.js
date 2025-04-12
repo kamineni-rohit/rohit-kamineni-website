dcac4834a518984d9909edea65ff023c3cb10963
Rohit Kamineni <kaminenirohit1@gmail.com>
2025-04-12 19:41:12 -0400

    Initial setup with Tailwind v3

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#1d6fba",
+      },
+    },
+  },
+  plugins: [],
+}
